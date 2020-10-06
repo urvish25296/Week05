0c9f92537264ca88f425055a9ac8c3d8cf393029
urvish <urvish25296@gmail.com>
2020-10-06 19:33:55 -0400

    including application source

---
+++ build/precache-manifest.fd599413047b3515b35e5318ea323aec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cfbb1917643aec3d3d8",
+    "url": "/static/css/main.b3b616d8.chunk.css"
+  },
+  {
+    "revision": "2cfbb1917643aec3d3d8",
+    "url": "/static/js/main.2cfbb191.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "e921be3ab6726eb5c9da7816a4026ccf",
+    "url": "/index.html"
+  }
+];
